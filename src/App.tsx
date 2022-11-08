520a08704e5b7b7aac5344764747111d53c536cc
RN Kushwaha <rn.kushwaha022@gmail.com>
2022-11-08 17:13:56 +0530

    added MUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { Container, Button, Box, Typography } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        React Dashboard
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App example
+        </Typography>
+        <Button variant="contained" color="primary" >Test</Button>
+        <LoadingButton
+          loading={true}
+          loadingPosition="start"
+          startIcon={<SaveIcon />}
+          variant="outlined"
+        >
+          Save
+        </LoadingButton>
+
+      </Box>
+    </Container>
   );
 }
 
